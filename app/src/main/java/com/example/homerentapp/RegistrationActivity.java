eeb371a04e5a92e2fa97432a0fedbfe06053647c
KeyurAK <keyurakbari2005@gmail.com>
2024-07-22 15:28:55 +0530

    RegistrationActivity ui changes

--- app/src/main/java/com/example/homerentapp/RegistrationActivity.java
+++ app/src/main/java/com/example/homerentapp/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.homerentapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.homerentapp.databinding.ActivityRegistrationBinding;
+
+import java.lang.reflect.Array;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    private ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String[] arrUserType = {"User", "Broker"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, arrUserType);
+        binding.spUserType.setAdapter(adapter);
+
+
     }
 }
