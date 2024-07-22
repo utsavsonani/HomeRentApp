fb7e4ef15f40a43d59536fad317b26e22f55b12f
Utsav <utsavsonani8654@gmail.com>
2024-07-22 19:19:56 +0530

    Home And Drawer is worked

--- app/src/main/java/com/example/homerentapp/HomeActivity.java
+++ app/src/main/java/com/example/homerentapp/HomeActivity.java
@@ -1,24 +1,37 @@
 package com.example.homerentapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton imageButtonToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        drawerLayout   = findViewById(R.id.drawerLayout);
+        imageButtonToggle = findViewById(R.id.buttonDrawerToggle);
+
+        imageButtonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
         });
     }
 }
