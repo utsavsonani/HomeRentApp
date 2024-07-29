f9aca43adcde721c27a185afa5fb2e59692b2a79
Utsav <utsavsonani8654@gmail.com>
2024-07-29 22:48:20 +0530

    Drawer Menu item Listener set  3.0

--- app/src/main/java/com/example/homerentapp/HomeActivity.java
+++ app/src/main/java/com/example/homerentapp/HomeActivity.java
@@ -13,6 +13,10 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.homerentapp.databinding.ActivityHomeBinding;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +34,7 @@ public class HomeActivity extends AppCompatActivity{
         EdgeToEdge.enable(this);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        
+
         SharedPreferences.Editor sharedPre = getSharedPreferences(getString(R.string.pref_file_key),MODE_PRIVATE).edit();
         auth = FirebaseAuth.getInstance();
 
@@ -49,38 +53,45 @@ public class HomeActivity extends AppCompatActivity{
                 Log.d("akak", "" + item);
                 Log.d("akak", "" + item.getItemId());
                 if(item.getItemId() == R.id.Home){
-                    Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
                     return true;
                 } 
                 else if(item.getItemId() == R.id.Profile){
-                    Toast.makeText(HomeActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new ProfileFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Nearby){
-                    Toast.makeText(HomeActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new NearbyFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Saved){
-                    Toast.makeText(HomeActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new SavedFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Notification){
-                    Toast.makeText(HomeActivity.this, "Notification", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new NotificationFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Message){
-                    Toast.makeText(HomeActivity.this, "Message", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new ProfileFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Setting){
-                    Toast.makeText(HomeActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new SettingFragment());
+                    drawerLayout.close();
                     return true;
                 }
                 else if(item.getItemId() == R.id.Help){
-                    Toast.makeText(HomeActivity.this, "Help", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new ProfileFragment());
+                    drawerLayout.close();
                     return true;
                 }
-                else if(item.getItemId() == R.id.Logout){
+                else if(item.getItemId() == R.id.Logoout){
+
                     auth.signOut();
                     sharedPre.putBoolean(getString(R.string.login_flag_key),false);
                     sharedPre.apply();
@@ -97,5 +108,11 @@ public class HomeActivity extends AppCompatActivity{
 
     }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container,fragment);
+        fragmentTransaction.commit();
+    }
 
 }
